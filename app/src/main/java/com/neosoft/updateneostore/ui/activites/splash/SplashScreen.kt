380273072fd6443ff6580a51d42ef52d5c0c2d76
Lokesh8602 <lokeshpatil8602@gmail.com>
2023-01-04 12:31:41 +0530

    Splash Activity

---
+++ app/src/main/java/com/neosoft/updateneostore/ui/activites/splash/SplashScreen.kt
@@ -0,0 +1,24 @@
+package com.neosoft.updateneostore.ui.activites.splash
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.neosoft.updateneostore.MainActivity
+import com.neosoft.updateneostore.R
+import kotlinx.android.synthetic.main.activity_splash_screen.*
+
+class SplashScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        logo.alpha = 0f
+        logo.animate().setDuration(1500).alpha(1f).withEndAction{
+            val i = Intent(this, MainActivity::class.java)
+            startActivity(i)
+            overridePendingTransition(android.R.anim.fade_in,android.R.anim.fade_out)
+            finish()
+        }
+    }
+
+}
